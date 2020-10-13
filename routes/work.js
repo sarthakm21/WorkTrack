f96b21efe5d5aa7fa50857e254c76e64d6b526a9
Elemento24 <mittalvishesh021@gmail.com>
2020-10-13 12:47:35 +0530

    Add MVC Strcuture for the Work Routes

--- routes/work.js
+++ routes/work.js
@@ -1,21 +1,17 @@
 const express = require('express'),
     router = express.Router(),
-    Work = require('../models/userWork'),
     isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get("/addwork", isLoggedIn, (req, res) => {
-    res.render("addwork");
-});
+const { getWork, postHome } = require('../controllers/workController');
 
-router.post("/home", isLoggedIn, (req, res) => {
-    Work.create(req.body.post, (err, done) => {
-        if (err) {
-            console.log("Error occured while posting to home", err);
-            res.redirect("/home");
-        } else {
-            res.redirect("/home");
-        }
-    });
-});
+// ADD WORK ROUTE
+router
+    .route("/addwork", isLoggedIn)
+    .get(getWork);
+
+// POST HOME ROUTE
+router
+    .route("/home", isLoggedIn)
+    .post(postHome);
 
 module.exports = router;
