ef2bf277d464500eaaa0b09fa9116a93f5fa452e
Elemento24 <mittalvishesh021@gmail.com>
2020-10-13 12:50:34 +0530

    Add MVC Structure for Stats Route

--- routes/statistics.js
+++ routes/statistics.js
@@ -1,16 +1,12 @@
 const express = require('express'),
     router = express.Router(),
-    Work = require('../models/userWork'),
     isLoggedIn = require('../middleware/isLoggedIn');
 
-router.get("/statistics", isLoggedIn, (req, res) => {
-    Work.find({ author: req.user._id, startTime: { "$gte": new Date(Date.now() - 1000 * 60 * 60 * 24 * 30), "$lt": new Date() } }, null, { sort: { startTime: 1 } }, (err, final) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("statistics", { final: final });
-        }
-    })
-});
+const { getStats } = require('../controllers/statController');
+
+// GET STATS ROUTE
+router
+    .route("/statistics", isLoggedIn)
+    .get(getStats);
 
 module.exports = router;
