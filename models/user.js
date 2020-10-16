1fd8fd0f86b484c00f1c327ee9002ae2ffb29821
Elemento24 <mittalvishesh021@gmail.com>
2020-10-16 23:38:43 +0530

    Change the User Model to include the New Fields

--- models/user.js
+++ models/user.js
@@ -4,7 +4,16 @@ const passportLocalMongoose = require('passport-local-mongoose');
 var UserSchema = new mongoose.Schema({
     username: String,
     email: String,
-    password: String
+    password: String,
+    imageURL: String,
+    about: String,
+    facebook: String,
+    instagram: String,
+    twitter: String,
+    works: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'work'
+    }]
 });
 
 UserSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
